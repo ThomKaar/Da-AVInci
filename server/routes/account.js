4478c4b7e789ddaddceaa6ba7eca76fcba13c096
Thomas Kaar <Thomkaar@gmail.com>
2019-11-27 23:51:34 -0600

    Update account.js

--- server/routes/account.js
+++ server/routes/account.js
@@ -7,3 +7,12 @@ accountRouter.get('/username=:username', AccountController.getAccountByUser);
 accountRouter.get('/id:=id', AccountController.getAccountById);
 
 module.exports = router;
+
+
+
+// Sweet
+
+// Sweet are her hands
+// For her fingertips curl into threads and cords of silk
+// For her knuckles have been grazed with technicolor spots
+// For her palms in the cool of morning lay open inviting mine
